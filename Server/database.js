1f2f04bdd104530fd3e03fcdcb88a52fb54d8c9c
harsh <harsh97patidar@gmail.com>
2023-05-14 15:46:40 +0530

    added ssl for db

--- Server/database.js
+++ Server/database.js
@@ -9,6 +9,7 @@ const client = new Client({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: true,
 });
 
 client.connect(function (err) {
